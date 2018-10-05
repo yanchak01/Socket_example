501a71b3038b2471085c399c13e58fc922427d64
Yanchak Andrii <a.yanchak01@gmail.com>
2018-10-05 19:00:31 +0300

    code

---
+++ Chat_client/Twitter_example/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Chat_client
+{
+    class Client
+    {
+        static Socket mySocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            mySocket.Connect("192.168.0.103", 904);
+
+            string message = Console.ReadLine();
+
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+
+            mySocket.Send(buffer);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Chat_server/Chat_server/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Net;
+
+
+namespace Chat_server
+{
+    class Program
+    {
+        static Socket mySocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 904);
+            mySocket.Bind(iPEndPoint);
+            mySocket.Listen(10); // кількість людей
+            Socket client = mySocket.Accept();
+
+            byte[] buffer = new byte[1024];
+
+            client.Receive(buffer);
+
+            string message = Encoding.ASCII.GetString(buffer);
+
+            Console.WriteLine(message);
+            Console.ReadKey();
+
+        }
+    }
+}
